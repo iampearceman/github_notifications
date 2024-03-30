4010fe17b39a2b730616d35a41feb3afba6ce43b
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-03-30 14:03:06 +0300

    init

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json()); // Parses JSON payload
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/webhook', (req, res) => {
+  console.log('Webhook received!');
+  console.log(req.body); // Here you can process the GitHub event data
+
+  // Respond to GitHub that the webhook was successfully received
+  res.status(200).send('Event received');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
